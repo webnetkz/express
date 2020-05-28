98cf3d4e057e638b515ba5751509894b1da6f01b
webnetkz <8gg001@gmail.com>
2020-05-28 17:24:53 +0600

    update

--- public/js/getQRscanner.js
+++ public/js/getQRscanner.js
@@ -2,14 +2,14 @@
 function getQRscanner() {
 
     let xhr = new XMLHttpRequest();
-    let content = document.querySelector('#content');
+    let x = document.querySelector('.resultQR');
 
     xhr.open('GET', '/app/getQRscanner.php');
     xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
 
     xhr.onreadystatechange = function() {
         if(xhr.readyState === 4 && xhr.status === 200) {
-            content.innerHTML = xhr.responseText;
+            x.innerHTML = xhr.responseText;
         }
     }
     xhr.send();

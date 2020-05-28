c07ab319e7a5d5c3c4dcc0f131283a497ea68f62
webnetkz <8gg001@gmail.com>
2020-05-28 17:32:14 +0600

    update

--- public/js/getQRscanner.js
+++ public/js/getQRscanner.js
@@ -1,17 +1,4 @@
-﻿// Сканирование QR кода
-function getQRscanner() {
-
-    let xhr = new XMLHttpRequest();
-    let x = document.querySelector('.resultQR');
-
-    xhr.open('GET', '/app/getQRscanner.php');
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-    xhr.onreadystatechange = function() {
-        if(xhr.readyState === 4 && xhr.status === 200) {
-            x.innerHTML = xhr.responseText;
-        }
-    }
-    xhr.send();
+﻿let resultQR = document.querySelector('.resultQR');
+if(resultQR != null) {
+    document.body.innerHTML = resultQR;
 }
-getQRscanner();
